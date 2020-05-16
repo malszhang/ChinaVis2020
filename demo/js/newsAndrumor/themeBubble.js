076aaaaab1be0915e4d5ce3019e35beb04e3d2a9
zy_real <320235617@qq.com>
2020-05-16 20:12:03 +0800

    调整目录

---
+++ demo/js/newsAndrumor/themeBubble.js
@@ -0,0 +1,84 @@
+var theme_padding = {
+    top: 5,
+    left: 5,
+    bottom: 5,
+    right: 5
+};
+
+$(document).ready(function () {
+    let width = document.getElementById("themeBubble").offsetWidth - theme_padding.left - theme_padding.right;
+    let height = document.getElementById("themeBubble").offsetHeight - theme_padding.top - theme_padding.bottom;
+    let svg3 = d3.select("#themeBubble")
+        .append("svg")
+        .attr("id", "theme_svg")
+        .attr("width", width)
+        .attr("height", height);
+
+    d3.json("js/newsAndrumor/data/themeBubble.json", function (error, root) {
+        // 定义布局方式
+        let pack = d3.pack()
+            .size([width, height])
+            .padding(2);
+
+        let data = d3.hierarchy(root)
+            .sum(function (d) {
+                return d.weight;
+            });
+        let nodes = pack(data).descendants();
+        drawThemeBubble(nodes, pack);
+    })
+});
+
+
+function drawThemeBubble(nodes, pack) {
+    let color = d3.scaleOrdinal(d3.schemeCategory20);
+
+    let bubbles = d3.select("#theme_svg").selectAll(".bubble")
+        .data(nodes)
+        .enter()
+        .filter(function (d) {
+            return !d.children;
+        })
+        .append("g")
+        .attr("class", "bubble")
+        .on("click",bubble_click);
+
+    bubbles.append("circle")
+        .style("fill", function (d, i) {
+            return color(i);
+        })
+        .attr("cx", function (d) {
+            return d.x;
+        })
+        .attr("cy", function (d) {
+            return d.y;
+        })
+        .attr("r", function (d) {
+            return d.r;
+        })
+        .append("svg:title")
+        .text(function(d){
+            return d.data.name + ":" + d.data.weight;
+        });
+
+    bubbles.append("text")
+        .attr("dy", ".2em")
+        .style("text-anchor", "middle")
+        .attr("x", function (d) {
+            return d.x;
+        })
+        .attr("y", function (d) {
+            return d.y;
+        })
+        .text(function (d) {
+            return d.data.name.substring(0, d.r / 3);
+        })
+        .style("font-size",function(d){
+            // 这里需要更一步的计算
+            return d.r / 2.5;
+        });
+}
+
+function bubble_click(d){
+    
+}
---
+++ demo/js/newsOfarea/heatMap.js
@@ -0,0 +1,170 @@
+var heat_padding = {
+    left: 15,
+    top: 5,
+    bottom: 5,
+    right: 5
+};
+var i = 0;
+var rootData;
+$(document).ready(function () {
+    let width = document.getElementById("heatMap").offsetWidth - heat_padding.left - heat_padding.right;
+    let height = document.getElementById("heatMap").offsetHeight - heat_padding.top - heat_padding.bottom;
+    let svg = d3.select("#heatMap")
+        .append("svg")
+        .attr("height", height)
+        .attr("width", width)
+        .append("g")
+        .attr("transform", "translate(" + heat_padding.left + "," + heat_padding.top + ")")
+        .attr("id", "heat_svg");
+    d3.json("js/newsOfarea/data/heatmap.json", function (error, treeData) {
+        drawHeatMap_tree(treeData, width, height);
+    });
+});
+// 绘制省份树图
+function drawHeatMap_tree(treeData, width, height) {
+    let tree = d3.tree()
+        .size([height, width]);
+
+    let root = d3.hierarchy(treeData, function (d) {
+        return d.children;
+    });
+    rootData = tree(root);
+    root._x = height / 2;
+    root._y = heat_padding.left;
+    update(root);
+}
+// 绘制、更新树图
+function update(source) {
+    let svg = d3.select("#heat_svg");
+
+    let nodes = rootData.descendants(),
+        links = rootData.descendants().slice(1);
+
+    nodes.forEach(function (d) {
+        d.y = d.depth * 100;
+    });
+
+    //  ***********节点绘制*********   
+    let node = svg.selectAll("g.node")
+        .data(nodes, function (d) {
+            return d.id || (d.id = ++i);
+        });
+    // ********新增加的节点*********
+    let nodeEnter = node.enter()
+        .append("g")
+        .attr("class", "node")
+        .attr("transform", function (d) {
+            return "translate(" + source._y + "," + source._x + ")";
+        })
+        .on("click", click);
+
+    nodeEnter.append("circle")
+        .attr("class", "node")
+        .attr("r", 10)
+        .style("fill", "#fff");
+
+    nodeEnter.append("text")
+        .attr("y", function (d) {
+            return d.children || d._children ? -18 : 18;
+        })
+        .attr("dy", ".35em")
+        .attr("text-anchor", "middle")
+        .text(function (d) {
+            return d.name;
+        })
+        .style("fill-opacity", 1);
+
+    // ************新增节点动画过渡*********
+    let nodeUpdate = nodeEnter.merge(node);
+
+    nodeUpdate.transition()
+        .duration(750)
+        .attr("transform", function (d) {
+            return "translate(" + d.y + "," + d.x + ")";
+        });
+
+    nodeUpdate.select("node.circle")
+        .style("fill", "#fff")
+        .attr("r", function (d) {
+            console.log(d);
+            return 10;
+        });
+
+    // ***********删除的节点********
+    let nodeExit = node.exit().transition()
+        .duration(750)
+        .attr("transform", function (d) {
+            return "translate(" + source.y + "," + source.x + ")";
+        })
+        .remove();
+    nodeExit.selectAll("node.circle")
+        .attr("r", 1e-6);
+    nodeExit.selectAll("node.text")
+        .style("fill-opacity", 1e-6);
+
+    //  ***********连线绘制*********
+    let link = svg.selectAll('path.link')
+        .data(links, function (d) {
+            return d.id;
+        });
+    // **************新增的连线************
+    let linkEnter = link.enter().insert('path', "g")
+        .attr("class", "link")
+        .attr('d', function (d) {
+            var o = {
+                x: source._x,
+                y: source._y
+            };
+            console.log(o);
+            return diagonal(o, o);
+        });
+
+    var linkUpdate = linkEnter.merge(link);
+    // ***********连线动画过渡*******
+    linkUpdate.transition()
+        .duration(750)
+        .attr('d', function (d) {
+            return diagonal(d, d.parent);
+        });
+    // ************删除的连线************
+    var linkExit = link.exit().transition()
+        .duration(750)
+        .attr('d', function (d) {
+            var o = {
+                x: source.x,
+                y: source.y
+            }
+            return diagonal(o, o);
+        })
+        .remove();
+    // ************保存当前节点的坐标***********
+    nodes.forEach(function (d) {
+        d._x = d.x;
+        d._y = d.y;
+    })
+}
+// 树图连线
+function diagonal(s, d) {
+    path = `M ${s.y} ${s.x}
+            C ${(s.y + d.y) / 2} ${s.x},
+              ${(s.y + d.y) / 2} ${d.x},
+              ${d.y} ${d.x}`
+
+    return path;
+}
+// 树图点击进行收放
+function click(d) {
+    if (d.children) {
+        d._children = d.children;
+        d.children = null;
+    } else {
+        d.children = d._children;
+        d._children = null;
+    }
+    update(d);
+}
+
+function drawHeatMap_heat(data){
+    
+}
+
