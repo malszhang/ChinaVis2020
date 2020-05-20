909c868ba45d5c059c9611200afd9e8ec6f7061f
舟遥遥以轻飏 <320235617@qq.com>
2020-05-20 22:06:13 +0800

    节点矩阵分割

--- NewsAndRumour/js_draw/tAnalyst/nodeMatrix.js
+++ NewsAndRumour/js_draw/tAnalyst/nodeMatrix.js
@@ -1,18 +1,22 @@
 function splitDiv(divNum) {
     let html = '';
-    let rowNum = Math.sqrt(divNum);
-    console.log(rowNum);
-    var height = document.getElementById("nodeMatrix").offsetHeight;
-    let cellHeight = (height - 48) / rowNum;
+    let rowNum = Math.ceil(divNum / 2);
+    let height = document.getElementById("nodeMatrix").offsetHeight;
+    $("#nodeMatrix").css({"height":height});
+    let cellHeight = (height - 20) / 2;
+    index = 0;
     for (let i = 0; i < rowNum; ++i) {
-        let row = '<el-row :gutter="16">';
-        for (let j = 0; j < rowNum; ++j) {
-            row += '<el-col :span="' + 24 / rowNum + '">';
-            row += '<div class="grid-content-header grid-content3 bg-purple" style="height:' + cellHeight + '" id="' + i + '-' + j + '"></div>';
+        let row = '<el-row :gutter="8" style="margin:5px">';
+        for (let j = 0; j < 2; ++j) {
+            row += '<el-col :span="12" style="">';
+            row += '<div class="grid-content-header grid-content3 bg-purple" style="border:0px;height:' + cellHeight + '" id="node-' + index + '"></div>';
             row += '</el-col>';
+            index++;
+            if(index >= divNum) break;
         }
         row += '</el-row>';
         html += row;
+        if(index >= divNum) break;
     }
     $("#nodeMatrix").html(html);
     new Vue({
@@ -20,19 +24,19 @@ function splitDiv(divNum) {
     });
 }
 
-function drawNodeMatrix() {
-    let dom = document.getElementById('0-0');
-    let myChart = echarts.init(dom);
-    $.getJSON("../tAnalyst/data/nodeData.json", function (data) {
+function drawNodeMatrix(dataNum, data) {
+    for(let i = 0;i<dataNum;++i){
+        let dom = document.getElementById('node-' + i);
+        let myChart = echarts.init(dom);
         drawNode(data, myChart);
-    });
-
+    }
 }
 
 function drawNode(webkitDep, myChart) {
     let option = {
         legend: {
-            data: ['网格关系'] //此处的数据必须和关系网类别中name相对应  
+            left: 5,
+            data: ['网格关系',"关系1", "关系2", "关系3",] //此处的数据必须和关系网类别中name相对应  
         },
         series: [{
             type: 'graph',
@@ -40,7 +44,7 @@ function drawNode(webkitDep, myChart) {
             animation: false,
             label: {
                 normal: {
-                    show: true,
+                    show: false,
                     position: 'right'
                 }
             },
