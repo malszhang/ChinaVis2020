21097e0b1c42799a426dae2b55cac350b910921b
userzhang <1482901238@qq.com>
2020-06-02 16:05:02 +0800

    阶梯图以及基础数据测试

--- NewsAndRumour/js_draw/eAnalyst/drawStair.js
+++ NewsAndRumour/js_draw/eAnalyst/drawStair.js
@@ -1,63 +1,84 @@
-function drawStair(idName){
+function drawStair(idName, title, data, time) {
 	var dom = document.getElementById(idName);
 	var myChart = echarts.init(dom);
-	var xAxisData = [];
-	var data1 = [];
-	var data2 = [];
-	for (var i = 0; i < 100; i++) {
-	    xAxisData.push('类目' + i);
-	    data1.push((Math.sin(i / 5) * (i / 5 -10) + i / 6) * 5);
-	    data2.push((Math.cos(i / 5) * (i / 5 -10) + i / 6) * 5);
-	}
+	// var xAxisData = [];
+	// var data1 = [];
+	// var data2 = [];
+	// for (var i = 0; i < 100; i++) {
+	//     xAxisData.push('类目' + i);
+	//     data1.push({name:i, value:['类目' + i, (Math.sin(i / 5) * (i / 5 -10) + i / 6) * 5]});
+	//     data2.push({name:i, value:['类目' + i, (Math.sin(i / 5) * (i / 5 -10) + i / 6) * 5]});
+	// }
+	// for (var i = 0; i < 100; i++) {
+	//     xAxisData.push('类目' + i);
+	//     data1.push((Math.sin(i / 5) * (i / 5 -10) + i / 6) * 5);
+	//     data2.push((Math.sin(i / 5) * (i / 5 -10) + i / 6) * 5);
+	// }
 	option = {
-		color:["red"],
-	    title: {
-	        text: '柱状图动画延迟'
-	    },
-	    legend: {
-	        data: ['bar', 'bar2']
-	    },
-	    toolbox: {
-	        // y: 'bottom',
-	        feature: {
-	            magicType: {
-	                type: ['stack', 'tiled']
-	            },
-	            dataView: {},
-	            saveAsImage: {
-	                pixelRatio: 2
-	            }
-	        }
-	    },
-	    tooltip: {},
-	    xAxis: {
-	        data: xAxisData,
-	        splitLine: {
-	            show: false
-	        }
-	    },
-	    yAxis: {
-	    },
-	    series: [ {
-	        name: 'bar2',
-	        type: 'bar',
-	        data: data2,
-	        animationDelay: function (idx) {
-	            return idx * 10 + 100;
-	        }
-	    },{
-			name: 'scatter1',
-			type: 'scatter',
-			data: data2,
-			animationDelay: function (idx) {
-			    return idx * 10 + 100;
+		// color:["red"],
+		title: {
+			text: title
+		},
+		legend: {
+			data: []
+		},
+		toolbox: {
+			// y: 'bottom',
+			feature: {
+				magicType: {
+					type: ['stack', 'tiled']
+				},
+				dataView: {},
+				saveAsImage: {
+					pixelRatio: 2
+				}
 			}
-		}],
-	    animationEasing: 'elasticOut',
-	    animationDelayUpdate: function (idx) {
-	        return idx * 5;
-	    }
+		},
+		tooltip: {},
+		xAxis: {
+			data: time,
+			splitLine: {
+				show: false
+			}
+		},
+		yAxis: {},
+		series: [],
+		animationEasing: 'elasticOut',
+		animationDelayUpdate: function(idx) {
+			return idx * 5;
+		}
 	};
+	data.forEach(function(d) {
+		option.legend.data.push(d.name);
+		// d.forEach(function(t){
+		option.series.push({
+			name: d.name,
+			type: 'bar',
+			data: d.data,
+			barGap: 0,
+			barWidth: 10,
+			animationDelay: function(idx) {
+				return idx * 10 + 100;
+			}
+		})
+		// })
+		option.series.push({
+			name: d.name,
+			type: 'scatter',
+			data: d.data,
+			symbol: 'circle',
+
+			itemStyle: {
+				color: '#fff',
+				borderWidth: 2,
+				borderColor: '#f00'
+			},
+			// symbolSize: 10,
+			animationDelay: function(idx) {
+				return idx * 10 + 100;
+			}
+		})
+	})
 	if (option && typeof option === "object") {
 		myChart.setOption(option, true);
 	}
